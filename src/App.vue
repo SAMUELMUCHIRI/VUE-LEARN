5753754b3a0e10de9115a76f1e597bb1157559c8
SAMUELMUCHIRI <eng.njaumuchiri@gmail.com>
2025-10-16 19:32:37 +0300

    learning vue :into lesson3 vue mastery

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue'
+import socksGreenImage from './assets/images/socks_green.jpeg'
+import socksBlueImage from './assets/images/socks_blue.jpeg'
+const products = ref('Socks')
+const description = ref('A pair of warm, fuzzy socks')
+const image = ref(socksBlueImage)
+const url = ref('#')
+const inventory = ref(100)
+</script>
+
+<template>
+  <div class="nav-bar"></div>
+    <div class="product-display">
+        <div class="product-container">
+          <div class="product-image"> 
+            <a :href="url"><img :src="image"  ></a>
+          </div>
+          <div class="product-info">
+
+            <h1>{{ products }}</h1>
+            <p v-if="inventory > 10 ">In Stock</p>
+            <p v-else-if="inventory <= 10 && inventory > 0">Almost Sold out </p>
+            <p v-else>Out of Stock</p>
+            <p>{{ description }}</p>
+          </div>
+        </div>
+  </div>
+</template>
+
+<style scoped></style>
